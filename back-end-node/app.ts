9d75c7d8d0be1151f0bb25296ade46c8b5ea1920
marianagsoares <marianasoares.ti@gmail.com>
2023-03-25 13:25:35 -0300

    feat: add token when register user

--- back-end-node/app.ts
+++ back-end-node/app.ts
@@ -11,7 +11,7 @@ app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: false}));
 app.use(routes);
 
-const port = 3000
+const port = 3000;
 app.listen(port, function(){
     console.log("Server running");
 });

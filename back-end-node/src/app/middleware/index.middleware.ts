3f6f59047c84187a26b920504afa1aa8ac893072
marianagsoares <marianasoares.ti@gmail.com>
2023-04-05 19:54:29 -0300

    feat: refactor get and post methods

--- back-end-node/src/app/middleware/index.middleware.ts
+++ back-end-node/src/app/middleware/index.middleware.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from '../routers/auth.router';
 import userRouter from '../routers/user.router';
+import productRouter from '../routers/product.router';
 
 const router = Router();
 
 router.use(authRouter);
 router.use('/users', userRouter);
+router.use('/products', productRouter);
 
 export default (router);
--- back-end-node/src/db/database.ts
+++ back-end-node/src/db/database.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://127.0.0.1/register_system").then(() => {
+mongoose.connect("mongodb://127.0.0.1/mjv_school").then(() => {
     console.log("Conected to database");
   }).catch((error) => {
     console.log({ error: 'Cannot connect to database' });

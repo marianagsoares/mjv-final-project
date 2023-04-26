dd1c00178177c5c078f0103a62bf77b68cba0750
marianagsoares <marianasoares.ti@gmail.com>
2023-04-26 15:36:57 -0300

    feat: add validation with Joi reset_password method

--- src/app/schemas/user.schema.ts
+++ src/app/schemas/user.schema.ts
@@ -1,7 +1,11 @@
 import Joi from "joi";
 import { User } from "../models/user.model";
+import { ICredentials } from "../models/credentials.model";
 
 const validator = (schema: Joi.ObjectSchema<User>) => (requestBody: User) => 
+    schema.validate(requestBody);
+
+    const validatorResetPassword = (schema: Joi.ObjectSchema<ICredentials>) => (requestBody: ICredentials) => 
     schema.validate(requestBody);
 
 const createUserSchema =  Joi.object({
@@ -31,7 +35,14 @@ const forgotPasswordSchema = Joi.object({
     email: Joi.string().email().required()
 });
 
+const resetPasswordSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(12).alphanum().required(),
+    token: Joi.string().alphanum().required()
+});
+
 export const validateCreateUser = validator(createUserSchema);
 export const validateUpdateUser = validator(updateUserSchema);
 export const validateUserAuthentication = validator(authenticateUserSchema);
 export const validateForgotPassword = validator(forgotPasswordSchema);
+export const validateResetPassword = validatorResetPassword(resetPasswordSchema);
